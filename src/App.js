7eacc613cddeeed69b3d5b52b451b895d1381686
seows2 <seows2@naver.com>
2021-10-10 00:23:34 +0900

    feat: material-table 오버라이딩 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import MaterialTable, { MTableHeader, MTableToolbar } from 'material-table';
 
 function App() {
-  return '11';
+  return (
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        columns={[
+          { title: 'Adı', field: 'name' },
+          { title: 'Soyadı', field: 'surname' },
+          { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+          {
+            title: 'Doğum Yeri',
+            field: 'birthCity',
+            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+          },
+        ]}
+        data={[
+          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        ]}
+        title="Demo Title"
+        options={{ selection: true }}
+        components={{
+          Toolbar: (props) => <div>asd</div>,
+        }}
+        localization={{ pagination: { labelRowsPerPage: '페이지' } }}
+      />
+    </div>
+  );
 }
-
 export default App;
